545e9584afddec6d83e09d1a2edb59efdf6c49d1
Anatolii Borysov <borisano@gmail.com>
2022-08-24 13:03:23 +0100

    Finish html form

--- spec/views/calculator/index.html.erb_spec.rb
+++ spec/views/calculator/index.html.erb_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "calculator/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#index' do
+    before do
+
+    end
+    it 'renders elements' do
+      render
+      expect(rendered).to have_selector('#calculator')
+      expect(rendered).to have_selector('#results')
+
+      expect(rendered).to have_selector('#property-price')
+      expect(rendered).to have_selector('#annual-interest-rate')
+      expect(rendered).to have_selector('#payment-schedule')
+      expect(rendered).to have_selector('#payment-per-payment')
+    end
+  end
 end
