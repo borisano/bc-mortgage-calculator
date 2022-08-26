577a17c8cf1636cf4521dcf607fac932f05cef08
Anatolii Borysov <borisano@gmail.com>
2022-08-26 15:59:01 +0100

    Fix factory for Mortgage

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     property_price { 100000 }
     annual_interest_rate { 5.0 }
     payment_per_payment { 1000 }
+    down_payment { property_price * 0.2 }
     amortization_period { Mortgage::AMORTIZATION_PERIOD_OPTIONS.sample }
     payment_schedule { [:monthly, :bi_weekly, :acc_bi_weekly].sample }
   end
